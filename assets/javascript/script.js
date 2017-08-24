e222fe3e7dab153553bdd90418c7258cbbc4edd7
joeyfboone <joey@urgentmail.com>
2017-08-24 08:48:00 -0400

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 //Letter choices available
-var AiChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var AiChoices = ['madonna', 'sting', 'joey', 'hello', 'world'];
 
 //Setting starting values
 var wins = 0;
@@ -7,33 +7,36 @@ var losses = 0;
 var guessedLetters = [];
 var guessesLeft = 9;
 var guesses = 9;
-
-
-//START OVER
-var start_over = function () {
-    totalGuesses = 9;
-    guessedLetters = [];
-    guessesLeft = 9;
-
-    updateGuessesSelected();
-    updateLetterToGuess();
-    updateGuessesLeft();
-}
+var AiChoiceMasked = null
+var mask = "_";
+var letters = []
 
 //Allow 9 Guesses
+var updateLetterToGuess = function () {
+    this.AiGuess = this.AiChoices[Math.floor(Math.random() * this.AiChoices.length)];
+    console.log(AiGuess);
+
+var split = AiGuess;
+    for (var i = 0; i < split.length; i++) {
+        console.log(split.charAt(i));
+    }   
+    
+};
 var updateGuessesLeft = function () {
     //HTML for Guesses Left
     document.querySelector('#guessLeft').innerHTML = "Guesses Left: " + guessesLeft;
-    console.log(AiGuess);
-}
-var updateLetterToGuess = function () {
-    this.AiGuess = this.AiChoices[Math.floor(Math.random() * this.AiChoices.length)];
 
-};
+}
+
 var updateGuessesSelected = function () {
     // Display attempted guesses as series of letters. 
     document.querySelector('#let').innerHTML = "Your Guesses: " + guessedLetters.join(', ');
 };
+var updateCurrentWord = function () {
+   //HTML for current word
+   document.querySelector('#current_word').innerHTML = "Current Word: " + AiChoiceMasked;
+};
+
 
 //These trigger functions
 updateLetterToGuess();
@@ -48,23 +51,35 @@ document.onkeyup = function (event) {
     guessedLetters.push(userGuess);
     updateGuessesLeft();
     updateGuessesSelected();
-
+    updateCurrentWord();
     if (guessesLeft > 0) {
         if (userGuess == AiGuess) {
             wins++;
             document.querySelector('#wins').innerHTML = "Wins: " + wins;
-            alert("Wow, you are psychic!");
+            alert("Wow, you have been saved from hanging!");
             start_over();
         }
     } else if (guessesLeft == 0) {
         // Then we lose and we'll update the html to display the loss 
         losses++;
         document.querySelector('#losses').innerHTML = "Losses: " + losses;
-        alert("Sorry, you are not psychic?");
+        alert("Sorry, you have been hung?");
         // Then we'll startover. 
-        start_over();
+        start_over()}
     }
-};
+
+//START OVER
+var start_over = function () {
+    totalGuesses = 9;
+    guessedLetters = [];
+    guessesLeft = 9;
+
+    updateGuessesSelected();
+    updateLetterToGuess();
+    updateGuessesLeft();
+}
+    
+
 
 
 
